35d054a84f936276a27b8c1e1d7231ce73dcb616
Tori Enyart <torienyart@gmail.com>
2023-03-27 16:03:29 -0600

    Add response for single merchant request

--- spec/requests/merchants_spec.rb
+++ spec/requests/merchants_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'get all merchants response' do
+describe 'can create a merchants response' do
   before :each do
     @m1 = create(:merchant)
     @m2 = create(:merchant)
@@ -9,6 +9,7 @@ describe 'get all merchants response' do
     @m5 = create(:merchant)
 
   end
+
   it "GET /merchants" do
     get api_v1_merchants_path
     json = JSON.parse(response.body, symbolize_names: true)
@@ -19,5 +20,15 @@ describe 'get all merchants response' do
     expect(json[:data].first[:type]).to eq('merchant')
     expect(json[:data].first[:attributes][:name]).to eq(@m1.name)
     expect(json[:data].size).to eq(5)
+  end
+
+  it "GET /merchants/:id" do
+    get api_v1_merchant_path(@m1)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+    expect(json[:data][:id]).to eq("#{@m1.id}")
+    expect(json[:data][:type]).to eq('merchant')
+    expect(json[:data][:attributes][:name]).to eq(@m1.name)
   end
 end
