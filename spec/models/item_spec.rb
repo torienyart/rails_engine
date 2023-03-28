33913fa3d1093ebb977cbeaec53d488e22da21a7
Tori Enyart <torienyart@gmail.com>
2023-03-27 18:38:15 -0600

    Create relationship between item and invoice item

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Item do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :unit_price }
+    it { should validate_numericality_of :unit_price}
+  end
+
+  describe 'relationships' do
+    it { should have_many :invoice_items }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should belong_to :merchant }
+  end
+end
