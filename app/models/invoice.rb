d9e646623851509c5438e2d5eb7ebaf16191b50a
Tori Enyart <torienyart@gmail.com>
2023-03-27 18:42:52 -0600

    Create relationships and validations for all model objects

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,5 +3,5 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  belongs_to :merchants
+  belongs_to :merchant
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
-  
+  validates_presence_of :name, :description, :unit_price, :merchant_id
+  validates :unit_price, numericality: { only_float: true }
+
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  belongs_to :merchant
 end
