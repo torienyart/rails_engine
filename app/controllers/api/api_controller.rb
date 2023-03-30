6e98a4291c76ddd81f8cffe16fe585b8d0701e9b
Tori Enyart <torienyart@gmail.com>
2023-03-30 10:59:25 -0700

    Move rescue clause for 404 error to application controller

--- app/controllers/api/api_controller.rb
+++
@@ -1,3 +0,0 @@
-class Api::ApiController < ApplicationController
-  
-end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,30 +1,15 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    begin
-      render json: MerchantSerializer.new(Merchant.all)
-    rescue ActiveRecord::RecordNotFound => error
-      serialized_error = ErrorSerializer.new(error).serializable_hash[:data][:attributes]
-      render json: serialized_error, status: :not_found
-    end
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
     if params[:id]
-      begin
-        render json: MerchantSerializer.new(Merchant.find(params[:id]))
-      rescue ActiveRecord::RecordNotFound => error
-        serialized_error = ErrorSerializer.new(error).serializable_hash[:data][:attributes]
-        render json: serialized_error, status: :not_found
-      end
+      render json: MerchantSerializer.new(Merchant.find(params[:id]))
     elsif params[:item_id]
-      begin
-        item = Item.find(params[:item_id])
-        id = item.merchant.id
-        render json: MerchantSerializer.new(Merchant.find(id))
-      rescue ActiveRecord::RecordNotFound => error
-        serialized_error = ErrorSerializer.new(error).serializable_hash[:data][:attributes]
-        render json: serialized_error, status: :not_found
-      end
+      item = Item.find(params[:item_id])
+      id = item.merchant.id
+      render json: MerchantSerializer.new(Merchant.find(id))
     end
   end
 end
